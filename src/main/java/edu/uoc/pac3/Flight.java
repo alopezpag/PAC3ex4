60abfc0535c32ed2b713461cabe3fc45781dca1e
Arnau López <108700238+alopezpag@users.noreply.github.com>
2023-11-25 20:20:56 +0100

    Finished exercise 4

--- src/main/java/edu/uoc/pac3/Flight.java
+++ src/main/java/edu/uoc/pac3/Flight.java
@@ -1,14 +1,17 @@
 package edu.uoc.pac3;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import java.io.File;
+import java.io.FileReader;
 
 public class Flight {
 
-    private Passenger[] passengers;
+    private final Passenger[] passengers;
     private int numPassengers;
 
-    public Flight(File file) throws Exception{
+    public Flight(File file) throws Exception {
         passengers = new Passenger[50];
         populate(file);
     }
@@ -21,13 +24,25 @@ public class Flight {
         this.numPassengers = numPassengers;
     }
 
-    private void populate(File file) throws Exception{
-        //TODO
+    private void populate(File file) throws Exception {
+        CSVReader reader = new CSVReaderBuilder(new FileReader(file)).build();
+        int passengerIndex = 0;
+        String[] nextLine;
+
+        while ((nextLine = reader.readNext()) != null) {
+            Passenger passenger = new Passenger(
+                    nextLine[0], // name
+                    nextLine[1], // passport
+                    Integer.parseInt(nextLine[2]), // age
+                    Boolean.parseBoolean(nextLine[3]) // specialNeeds
+            );
+            // add passenger to array and increment index
+            passengers[passengerIndex++] = passenger;
+        }
+        setNumPassengers(passengerIndex);
     }
 
-    public Passenger[] getPassengers(){
+    public Passenger[] getPassengers() {
         return passengers;
     }
-
-
 }
